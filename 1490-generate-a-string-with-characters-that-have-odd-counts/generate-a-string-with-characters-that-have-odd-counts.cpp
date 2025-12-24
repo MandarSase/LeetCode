9911015fcf6cc1f496f0635a1a97db321cacc411
MandarSase <124461120+MandarSase@users.noreply.github.com>
2025-12-25 00:05:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (66.57%) - LeetSync

---
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        string ans="";
+        for(int i=0;i<n;i++){
+            ans+='o';
+        }
+        if(n%2==0){
+            ans[n-1]='h';
+        }   
+        return ans;
+    }
+};
