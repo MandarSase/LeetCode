f5a177540a3dc0cc1eb7890efe400c98945c6928
MandarSase <124461120+MandarSase@users.noreply.github.com>
2026-01-11 00:05:06 +0530

    Time: 2 ms (73.68%) | Memory: 17.3 MB (61.17%) - LeetSync

---
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> result(n, 0);
+        stack<int> st;
+        int prevTime = 0;
+
+        for (string log : logs) {
+            // Parse the log
+            int pos1 = log.find(':');
+            int pos2 = log.find(':', pos1 + 1);
+
+            int id = stoi(log.substr(0, pos1));
+            string type = log.substr(pos1 + 1, pos2 - pos1 - 1);
+            int time = stoi(log.substr(pos2 + 1));
+
+            if (type == "start") {
+                if (!st.empty()) {
+                    result[st.top()] += time - prevTime;
+                }
+                st.push(id);
+                prevTime = time;
+            } 
+            else { // "end"
+                result[st.top()] += time - prevTime + 1;
+                st.pop();
+                prevTime = time + 1;
+            }
+        }
+
+        return result;
+    }
+};
