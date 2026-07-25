a7f5fed1250f82d5edada4ba38937600c0369100
MandarSase <124461120+MandarSase@users.noreply.github.com>
2026-07-26 00:37:22 +0530

    Time: 3 ms (15.04%) | Memory: 10.6 MB (5.88%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+       vector<int> arr;
+       while (n > 0) {
+        arr.push_back(n % 10); 
+         n /= 10;               
+       }
+    sort(arr.rbegin(),arr.rend());
+    int ans=arr[0]*arr[1];
+
+
+
+
+   return ans;
+    }
+};
