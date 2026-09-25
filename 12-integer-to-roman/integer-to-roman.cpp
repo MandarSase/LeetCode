b096d4f0f8def7b1ff3befdd8a7a30b1289dfc26
MandarSase <124461120+MandarSase@users.noreply.github.com>
2026-09-26 00:35:25 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (99.32%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -5,21 +5,17 @@ public:
             1000, 900, 500, 400, 100, 90, 50,
             40, 10, 9, 5, 4, 1
         };
-
         static const string symbols[] = {
             "M", "CM", "D", "CD", "C", "XC", "L",
             "XL", "X", "IX", "V", "IV", "I"
         };
-
         string ans;
-
         for (int i = 0; i < 13 && num > 0; i++) {
             while (num >= values[i]) {
                 num -= values[i];
                 ans += symbols[i];
             }
         }
-
         return ans;
     }
 };
